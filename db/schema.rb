0dbdd1c087ca936d7d1ebb25a55ba931dcf555e2
Connor Flynn <connorflynn95@hotmail.com>
2020-11-04 11:25:20 +1100

    ignore

--- db/schema.rb
+++ db/schema.rb
@@ -50,6 +50,11 @@ ActiveRecord::Schema.define(version: 2020_11_03_012449) do
     t.string "title"
   end
 
+  create_table "games_users", id: false, force: :cascade do |t|
+    t.bigint "game_id", null: false
+    t.bigint "user_id", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
